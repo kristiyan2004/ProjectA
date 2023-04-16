140eec18bdf436a5eaa0804ec81207713b237c44
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2023-04-16 19:37:27 +0300

    Changed Main() method content.

--- ProjectA/StartUp.cs
+++ ProjectA/StartUp.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Employee firstEmployee = new Employee("Maria", "Hristova", 18, 'F', 5);
+            Employee firstEmployee = new Employee("Maria", "Hristova", 24, 'F', 1);
             firstEmployee.Introduce();
             firstEmployee.GoToWork();
+
+            Employee secondEmployee = new Employee("Kristiyan", "Hristov", 35, 'M', 5);
+            secondEmployee.Introduce();
+            secondEmployee.GoToWork();
 
             Student student = new Student("Ivan", "Ivanov", 14, 'M', 8);
             student.Introduce();
